8be918f5c97753c7e53f844c0304a40aab5c2e40
Yasmin Lopes <yclopes27@gmail.com>
2023-10-05 12:01:24 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
+import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cadastro-seguro'
+  },
+  {
+    path: 'cadastro-seguro',
+    component: CadastroSeguroComponent
+  },
+  {
+    path: 'listar-seguros',
+    component: ListarSegurosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

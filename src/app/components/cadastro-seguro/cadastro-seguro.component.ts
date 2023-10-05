1f8a68b6b6446f39c70bd6fb7e34bd859d75824d
Yasmin Lopes <yclopes27@gmail.com>
2023-10-05 14:03:43 -0300

    feat: implement observable to list cars marks

--- src/app/components/cadastro-seguro/cadastro-seguro.component.ts
+++ src/app/components/cadastro-seguro/cadastro-seguro.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MarcaCarro } from 'src/app/models/marca-carro.model';
+import { Seguro } from 'src/app/models/seguro-model';
+import { MarcaCarroService } from 'src/app/services/marca-carro/marca-carro.service';
 
 @Component({
   selector: 'app-cadastro-seguro',
   templateUrl: './cadastro-seguro.component.html',
-  styleUrls: ['./cadastro-seguro.component.scss']
+  styleUrls: ['./cadastro-seguro.component.scss'],
 })
-export class CadastroSeguroComponent {
+export class CadastroSeguroComponent implements OnInit {
+  public seguro = new Seguro();
+  public marcasCarro$: Observable<MarcaCarro[]>;
 
+  constructor(private marcaCarroService: MarcaCarroService) {}
+
+  ngOnInit(): void {
+    this.listarMarcas()
+  }
+
+  private listarMarcas() {
+    this.marcasCarro$ = this.marcaCarroService.getMarcas()
+  }
+
+  public enviarNotificacao() {
+
+  }
+
+  public adicionar() {
+
+  }
 }

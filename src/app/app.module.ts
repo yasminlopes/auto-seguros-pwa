fc493ed27fb380793d98b217c9adcd34023c391d
Yasmin Lopes <yclopes27@gmail.com>
2023-10-05 12:01:39 -0300

    feat: add HttpClient imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -8,6 +7,11 @@ import { MenuComponent } from './components/menu/menu.component';
 import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
 import { FormsModule } from '@angular/forms';
 import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientJsonpModule,
+  HttpClientModule,
+} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,10 +27,13 @@ import { ListarSegurosComponent } from './components/listar-seguros/listar-segur
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

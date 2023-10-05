3284a67ccb64bdf673c2c0aa201973a0b9a2013d
Yasmin Lopes <yclopes27@gmail.com>
2023-10-05 12:19:25 -0300

    feat: create models

---
+++ src/app/models/seguro-model.ts
@@ -0,0 +1,10 @@
+import { MarcaCarro } from './marca-carro.model';
+
+export class Seguro {
+  marcaCarro: MarcaCarro;
+  modeloCarro: string;
+  placaCarro: string;
+  nomeProprietario: string;
+  sobrenomeProprietario: string;
+  dataNascimentoProprietario: string;
+}

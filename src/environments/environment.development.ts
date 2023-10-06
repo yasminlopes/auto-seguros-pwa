af4803d5da0e909a344e56f5cc28323e3d8ac785
Yasmin Lopes <yclopes27@gmail.com>
2023-10-06 14:21:53 -0300

    feat: add enviroments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  version: '1.0',
+  API_CARROS: 'https://www.carqueryapi.com/api/0.3/?callback=?&',
+  API: 'http://localhost:9000',
+  VAPID_PUBLIC_KEY:
+    'BGs-GrCwDhu1z_3yscq8yonxiZmGT2CBwRynJi9dFSpeynvPrrmeVVJ3YgzGBgTmgxb6qEg2p_3-nRWjGYdBm1k',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  version: '1.0',
+  API_CARROS: 'https://www.carqueryapi.com/api/0.3/?callback=?&',
+  API: 'vercel',
+  VAPID_PUBLIC_KEY:
+    'BGs-GrCwDhu1z_3yscq8yonxiZmGT2CBwRynJi9dFSpeynvPrrmeVVJ3YgzGBgTmgxb6qEg2p_3-nRWjGYdBm1k',
+};

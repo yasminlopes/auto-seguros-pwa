225305b85723e41eba4e784811105f22ea0889e7
Yasmin Lopes <yclopes27@gmail.com>
2023-10-06 11:47:52 -0300

    feat: add fake server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { listarSeguros, salvarSeguro } = require('./seguro-service');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+app.route('/api/seguro').post(salvarSeguro);
+
+app.route('/api/seguros').get(listarSeguros);
+
+const PORT = 9000;
+const HOST = 'localhost';
+
+const httpServer = app.listen(PORT, HOST, () => {
+  console.log('HTTP Server running at http://' + HOST + ':' + PORT);
+});
---
+++ server/seguro-service.js
@@ -0,0 +1,13 @@
+const { SEGUROS } = require('./in-memory-db');
+
+exports.salvarSeguro = (req, res) => {
+  const seguro = req.body;
+console.log(seguro)
+  SEGUROS.push(seguro);
+
+  res.status(200).json({ message: 'Seguro adicionado com sucesso!' });
+}
+
+exports.listarSeguros = (req, res) => {
+  res.status(200).json(SEGUROS);
+}

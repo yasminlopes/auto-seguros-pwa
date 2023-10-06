3a38dc0359830331c9cefd745e24f5204238ffd6
Yasmin Lopes <yclopes27@gmail.com>
2023-10-06 11:50:54 -0300

    feat: handle secures listing

--- src/app/components/listar-seguros/listar-seguros.component.ts
+++ src/app/components/listar-seguros/listar-seguros.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Seguro } from 'src/app/models/seguro-model';
+import { SeguroService } from 'src/app/services/seguro/seguro.service';
 
 @Component({
   selector: 'app-listar-seguros',
   templateUrl: './listar-seguros.component.html',
   styleUrls: ['./listar-seguros.component.scss']
 })
-export class ListarSegurosComponent {
+export class ListarSegurosComponent implements OnInit {
+
+  public seguros$: Observable<Seguro[]>
+
+  constructor(
+    private seguroService: SeguroService
+  ) {}
+
+  ngOnInit() {
+    this.seguros$ = this.seguroService.listarSeguros()
+  }
 
 }

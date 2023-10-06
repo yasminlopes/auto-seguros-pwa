e980d69276485090b2e82d640f6cff8e7428d314
Yasmin Lopes <yclopes27@gmail.com>
2023-10-06 14:09:51 -0300

    feat: set vapid details

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const webpush = require('web-push')
 const { listarSeguros, salvarSeguro } = require('./seguro-service');
+
+const vapidKeys = {
+  publicKey: 'BGs-GrCwDhu1z_3yscq8yonxiZmGT2CBwRynJi9dFSpeynvPrrmeVVJ3YgzGBgTmgxb6qEg2p_3-nRWjGYdBm1k',
+  privateKey: 'THMG1jOP0QsMVgo5xB3zrhZ1UQ-0ZbWcQwgnQGFoqLo'
+}
+
+webpush.setVapidDetails(
+  'test@gmail.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
 
 const app = express();
 app.use(bodyParser.json());
